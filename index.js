50482f28b500d7cab1c4bc41933f76ec663977d8
Savannah Kolb <savannahnkolb@gmail.com>
2021-06-08 00:13:52 -0400

    It is working

--- index.js
+++ index.js
@@ -7,34 +7,82 @@ const Manager = require("./lib/Manager");
 const fs = require("fs");
 
 const managerPrompts = [
-            {
-                type: 'input',
-                name: 'name',
-                message: "Enter Manager's Name:",
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: "Enter Manager's ID Number:",
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: "Enter Manager's email address:",
-            },
-            {
-                type: "input",
-                name: "officeNumber",
-                message: "Enter Manager's current office number:",
-            }
-        ]
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter Manager's Name:",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter Manager's ID Number:",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter Manager's Email Address:",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter Manager's current office number:",
+    }
+]
 
- 
+const employeePrompts = [
+    {
+        type: 'list',
+        name: 'title',
+        message: "What is the title of your next employee?",
+        choices: ['Engineer', 'Intern'],
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter Employee's Name:",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter Employee's ID Number:",
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "Enter Employee's GitHub username:",
+        when: (answers) => answers.title === 'Engineer'
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "Enter Employee's School:",
+        when: (answers) => answers.title === 'Intern'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter Employees's email address:",
+    },
+];
+
 
 const askPrompts = () => {
     inquirer.prompt(managerPrompts).then((answers) => {
         const manager = new Manager(answers.name, answers.id, answers.officeNumber, answers.email)
         generateManager(manager);
+        addOrGenerate();
+    })
+}
+
+const  addEmployeePrompts= () => {
+    inquirer.prompt(employeePrompts).then((answers) => {
+        if (answers.title === 'Engineer') {
+            const engineer = new Engineer(answers.name, answers.id, answers.github, answers.email)
+            generateEngineer(engineer);
+        } else {
+            const intern = new Intern(answers.name, answers.id, answers.school, answers.email)
+            generateIntern(intern)
+        }
         addOrGenerate();
     })
 }
@@ -74,8 +122,48 @@ const generateManager = (manager) => {
                     </div>
                 </div>
             </div>`;
-            fs.writeFile('./dist/team.html', renderM, (err) => err ? console.log(err) : '')
+    fs.writeFile('./dist/team.html', renderM, (err) => err ? console.log(err) : '')
 
+};
+
+const generateEngineer = (engineer) => {
+    const renderE = `
+    <div class="col">
+        <div class="card" style="width: 15rem;">
+            <div class="card-header p-3 mb-2 bg-danger text-white">
+                <h4>Engineer</h4>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${engineer.getName()}</h5>
+                <ul>
+                    <li>ID: ${engineer.getId()}</li>
+                    <li><a href="https://github.com/${engineer.getGithub()}" target="_blank" class="card-link">GitHub: ${engineer.getGithub()}</a></li>
+                    <li><a href="mailto:${engineer.getEmail()}" class="card-link">${engineer.getEmail()}</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+    fs.appendFile('./dist/team.html', renderE, (err) => err ? console.log(err) : '')
+};
+
+const generateIntern = (intern) => {
+    const renderI = `
+    <div class="col">
+        <div class="card" style="width: 15rem;">
+            <div class="card-header p-3 mb-2 bg-danger text-white">
+                <h4>Intern</h4>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${intern.getName()}</h5>
+                <ul>
+                    <li>ID: ${intern.getId()}</li>
+                    <li>${intern.getSchool()}</li>
+                    <li><a href="mailto:${intern.getEmail()}" class="card-link">${intern.getEmail()}</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+    fs.appendFile('./dist/team.html', renderI, (err) => err ? console.log(err) : '')
 };
 
 const addOrGenerate = () => {
@@ -85,8 +173,8 @@ const addOrGenerate = () => {
         message: "Would you like to add any more employees to your team?",
         choices: ["Yes, I have more team members to add.", "No, my team is complete."],
     }]).then((answer) => {
-        if (answer.newTeamMem === 'Yes, I have more team members to add.') {
-            // addNewEmployee();
+        if (answer.newEmployee === 'Yes, I have more team members to add.') {
+            addEmployeePrompts();
         } else {
             console.log('Your Team Profile has been generated!');
             const endHTML = `
