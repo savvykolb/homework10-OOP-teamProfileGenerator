8d137653d1794a5e8aff3f6dc48235471ad95260
Savannah Kolb <savannahnkolb@gmail.com>
2021-06-07 20:42:21 -0400

    Pulling info into array - but not 'askprompts' q's

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
 const fs = require("fs");
-const Choices = require("inquirer/lib/objects/choices");
 
 let finalTeamArray = [];
 console.log('finalTeamArray:', finalTeamArray)
@@ -36,15 +35,33 @@ const askPrompts = () => {
                 name: 'role',
                 message: "Enter Employees's Role:",
                 choices: [
+                    "Manager",
                     "Engineer",
                     "Intern",
-                    "Manager",
                 ],
             },
         ])
         .then( //returns a promise - still need to be more comfortable in understanding promises. This link helped https://javascript.info/promise-basics
             function ({ name, id, email, role }) { //refering to the constructor object 
                 switch (role) { //switch statement
+                    case "Manager":
+                        inquirer
+                            .prompt({
+                                type: "input",
+                                name: "officeNumber",
+                                message: "Enter Manager's current office number:",
+                            }).then(function (data) {
+                                const name = data.name
+                                const id = data.id
+                                const email = data.email
+                                const officeNumber = data.officeNumber
+                                const teamMember = new Manager(name, id, email, officeNumber)
+                                finalTeamArray.push(teamMember)
+                                console.log('finalTeamArray:', finalTeamArray)
+                                addAnotherEmployee();
+                            }
+                            )
+                        break
                     case "Engineer":
                         inquirer
                             .prompt({
@@ -53,11 +70,12 @@ const askPrompts = () => {
                                 message: "Enter Engineer's GitHub username:",
                             }).then(function (data) {
                                 const name = data.name
-                                const id = finalTeamArray.length + 1
+                                const id = data.id
                                 const email = data.email
                                 const github = data.github
                                 const teamMember = new Engineer(name, id, email, github)
                                 finalTeamArray.push(teamMember)
+                                console.log('finalTeamArray:', finalTeamArray)
                                 addAnotherEmployee();
                             }
                             )
@@ -76,27 +94,12 @@ const askPrompts = () => {
                                 const school = data.school
                                 const teamMember = new Intern(name, id, email, school)
                                 finalTeamArray.push(teamMember)
+                                console.log('finalTeamArray:', finalTeamArray)
                                 addAnotherEmployee();
                             }
                             )
                         break
-                    case "Manager":
-                        inquirer
-                            .prompt({
-                                type: "input",
-                                name: "officeNumber",
-                                message: "Enter Manager's current office number:",
-                            }).then(function (data) {
-                                const name = data.name
-                                const id = 1
-                                const email = data.email
-                                const officeNumber = data.officeNumber
-                                const teamMember = new Manager(name, id, email, officeNumber)
-                                finalTeamArray.push(teamMember)
-                                addAnotherEmployee();
-                            }
-                            )
-                        break
+                    
                 }
             })
 }
@@ -114,12 +117,90 @@ const addAnotherEmployee = () => {
                 askPrompts();
                 break;
             case "No, my team is complete":
-                compileTeam();
-                break;
+                // inquirer
+                //     .prompt({
+                //         type: "input",
+                //         name: "teamname",
+                //         message: "Enter your Team Name:",
+                //     }).then(function (data) {
+                //         const teamName = data.teamname
+                //         finalTeamArray.push(teamName)
+                        compileTeam();
+                        break;
+                //     })
         }
     });
 }
 
+function compileTeam() {
+    console.log("//////////YOUR TEAM IS BEING CREATED!!////////")
+
+    const htmlArray = []
+    const htmlBeginning = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${finalTeamArray[0]}</title>
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <style>
+     ${style}
+    </style>
+</head>
+<body>
+    <div class="banner-bar">
+        <h1>${finalTeamArray[0]}</h1>
+    </div>
+    <div class="card-container">
+    `
+    htmlArray.push(htmlBeginning);
+
+    for (let i = 1; i < finalTeamArray.length; i++) {
+        let object = `
+        <div class="member-card">
+            <div class="card-top">
+                <h2>${finalTeamArray[i].name}</h2>
+                <h2>${finalTeamArray[i].title}</h2>
+            </div>
+            <div class="card-bottom">
+                <p>Employee ID: ${finalTeamArray[i].id}</p>
+                <p>Email: <a href="mailto:${finalTeamArray[i].email}">${finalTeamArray[i].email}</a>></p>
+        `
+        if (finalTeamArray[i].officeNumber) {
+            object += `
+            <p>${finalTeamArray[i].officeNumber}</p>
+            `
+        }
+        if (finalTeamArray[i].github) {
+            object += `
+            <p>GitHub: <a href="https://github.com/${finalTeamArray[i].github}">${finalTeamArray[i].github}</a></p>
+            `
+        }
+        if (finalTeamArray[i].school) {
+            object += `
+            <p>School: ${finalTeamArray[i].school}</p>
+            `
+        }
+        object += `
+        </div>
+        </div>
+        `
+        htmlArray.push(object)
+    }
+
+    const htmlEnd = `
+    </div>
+    </body>
+    </html>
+    `
+    htmlArray.push(htmlEnd);
+
+    fs.writeFile(`./dist/${finalTeamArray[0]}.html`, htmlArray.join(""), function (err) {
+
+    })
+}
 
 
 askPrompts()
